b931fb5ea2cb6cbd190b40470fea3be3a74e06b3
Krausz Bertold <krauszbertold@gmail.com>
2020-08-13 23:04:00 +0200

    reworked stuff so Zoltan does not need to reload the whole page to get another Zoltan

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
 var API_KEY = '17871523-f4f6157e2d841a83f82aeeb38';
-var korlátozoltán = 20;
 console.log('CONSOLE-OZOLTÁN?')
+
 class Zoltán{
+
     constructor(string, keyword){
         this.string = string;
         this.keyword = keyword.replace(" ","+");
     };
 
-    getImageUrl(){
+    displayZoltán(){
+        var zoli = this;
         var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(this.keyword);
-        var imageUrl = "";
+        var zolImageURL  = "";
+        var korlátozoltán = 20;
 
         $.ajax({
             url: URL,
             dataType: 'json',
-            async: false,
+            async: true,
             success: function(data) {
+                $('#loadingimage').show();               
+                document.getElementById('title').innerHTML = '';
+
                 if (parseInt(data.totalHits) < korlátozoltán){
                     korlátozoltán = parseInt(data.totalHits) -1;
                 }
+                
+                $('#imgzoltan img').remove();
                 var sorszámozoltán = Math.floor(Math.random() * korlátozoltán);
-                imageUrl =  data.hits[sorszámozoltán].webformatURL;
+                var zolImage = new Image();
+                zolImageURL =  data.hits[sorszámozoltán].webformatURL;
+                zolImage.src = zolImageURL;
+                zolImage.onload = function() {
+                    document.getElementById('title').innerHTML = zoli.string;
+                    $('#loadingimage').hide();
+                }
+                $('#imgzoltan img').attr('src', "ajax-loader.gif");
+                document.getElementById("imgzoltan").appendChild(zolImage);
             }
-          });
-          return imageUrl;
+        });    
     }
 }
 
@@ -47,7 +62,7 @@ var listázoltán = [
     ,new Zoltán("HómOffizoltán?","home office")
     ,new Zoltán("Pofázoltán?","arguing")
     ,new Zoltán("Csajozoltán?","party girl")
-    ,new Zoltán("Pálinkázoltán?","rakija")
+    ,new Zoltán("Pálinkázoltán?","shots")
     ,new Zoltán("Vacsorázoltán?","last dinner")
     ,new Zoltán("Biliárdozoltán?","8 ball pool")
     ,new Zoltán("Zsebhokizoltán?","pocket")
@@ -59,13 +74,16 @@ var listázoltán = [
     ,new Zoltán("Hazudozoltán?","lying")
     ,new Zoltán("Bulizoltán?","party")       
     ,new Zoltán("Homokozoltán?","sand")       
-    ,new Zoltán("Légkondizoltán?","air conditioning")       
+    ,new Zoltán("Légkondizoltán?","air conditioning")   
+    ,new Zoltán("Társasozoltán?","board game")       
+    ,new Zoltán("Habozoltán?","hesitation")       
+    ,new Zoltán("Bújócskázoltán?","hide and seek")       
 ];
 
-var sorszámozoltán = Math.floor(Math.random() * (listázoltán.length));
-document.getElementById('title').innerHTML = listázoltán[sorszámozoltán].string;
+function loadZoltán(){
+    var sorszámozoltán = Math.floor(Math.random() * (listázoltán.length));
+    listázoltán[sorszámozoltán].displayZoltán();
+    setTimeout(function(){ loadZoltán(); }, 3000);
+}
 
-imageUrl =  listázoltán[sorszámozoltán].getImageUrl();
-console.log(imageUrl);
-document.getElementById("imgzoltan").src = imageUrl;
-setTimeout(function(){ location.reload(); }, 2500);
+loadZoltán();
